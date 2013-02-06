860b242695ffd6deb22fa92ef6f04e72978ba728
Eric Windisch <eric@cloudscaling.com>
2013-02-06 10:39:41 -0500

    Get pub key parameters. Support MPI.
    Also cleans up some pep8 problems.

--- opgp.py
+++ opgp.py
@@ -42,6 +42,7 @@ Packet types:
        60 to 63 -- Private or Experimental Values
 """
 
+
 def get_header(raw_data):
     header = ord(raw_data[0])
     fbval = header - 192
@@ -55,7 +56,7 @@ def get_header(raw_data):
         elif bl_header > 191 and bl_header < 8384:
             print "NewMedium"
             raw_length = struct.unpack('>H', raw_data[1:2])[0]
-            length = (raw_length ^ (192 << 8)) + 192
+            length = (raw_length ^ 49152) + 192
             return (tag, length, 3)
         elif bl_header == 255:
             print "NewLarge"
@@ -78,8 +79,8 @@ def get_header(raw_data):
             print "OldSmall"
             fmt = ('>B', '>H')[ltype == 1]
             octets = ltype + 1
-            length = struct.unpack(fmt, raw_data[1:1+octets])[0]
-            return (tag, length, ltype+2)
+            length = struct.unpack(fmt, raw_data[1:1 + octets])[0]
+            return (tag, length, ltype + 2)
         raise Exception("Invalid length type.")
     raise Exception("Invalid Packet")
 
@@ -99,6 +100,43 @@ def build_index(raw_data):
     return index
 
 
+def mpi(buf):
+    """
+    3.2. Multi-Precision Integers
+
+       Multi-Precision Integers (also called MPIs) are unsigned integers
+       used to hold large integers such as the ones used in cryptographic
+       calculations.
+
+       An MPI consists of two pieces: a two-octet scalar that is the length
+       of the MPI in bits followed by a string of octets that contain the
+       actual integer.
+
+       These octets form a big-endian number; a big-endian number can be
+       made into an MPI by prefixing it with the appropriate length.
+
+       Examples:
+
+       (all numbers are in hexadecimal)
+
+       The string of octets [00 01 01] forms an MPI with the value 1. The
+       string [00 09 01 FF] forms an MPI with the value of 511.
+
+       Additional rules:
+
+       The size of an MPI is ((MPI.length + 7) / 8) + 2 octets.
+
+       The length field of an MPI describes the length starting from its
+       most significant non-zero bit. Thus, the MPI [00 02 01] is not formed
+       correctly. It should be [00 01 01].
+    """
+    length = struct.unpack(">H", buf[0:2])[0]
+    print "Length of MPI: %i" % length
+    to_padded_hex = lambda n: '%0.2X' % ord(n)
+    inhexstr = ''.join(map(to_padded_hex, buf[2:2 + length]))
+    return (int('0x' + inhexstr, 0), ((length + 7) / 8) + 2)
+
+
 def read_packet(pkt_data):
     """
     A version 4 packet contains:
@@ -106,19 +144,6 @@ def read_packet(pkt_data):
      - A four-octet number denoting the time that the key was created.
      - A one-octet number denoting the public-key algorithm of this key.
      - A series of multiprecision integers comprising the key material.
-       This algorithm-specific portion is:
-       Algorithm-Specific Fields for RSA public keys:
-         - multiprecision integer (MPI) of RSA public modulus n;
-         - MPI of RSA public encryption exponent e.
-       Algorithm-Specific Fields for DSA public keys:
-         - MPI of DSA prime p;
-         - MPI of DSA group order q (q is a prime divisor of p-1);
-         - MPI of DSA group generator g;
-         - MPI of DSA public-key value y (= g**x mod p where x
-           is secret).
-       Algorithm-Specific Fields for Elgamal public keys:
-         - MPI of Elgamal prime p;
-         - MPI of Elgamal group generator g;
     """
     (ver, created_at, algo) = struct.unpack('>BIB', pkt_data[0:6])
     if ver != 4:
@@ -138,9 +163,42 @@ def read_packet(pkt_data):
            19         - Reserved for ECDSA
            20         - Elgamal (Encrypt or Sign)
     """
-    rsa = algo in (1, 2, 3)
-    dsa = algo == 17
-    elgamal = algo == 20
+
+    # extract integers comprising the key material.
+    if algo in (1, 2, 3):  # rsa
+        """
+        Algorithm-Specific Fields for RSA public keys:
+        - multiprecision integer (MPI) of RSA public modulus n;
+        - MPI of RSA public encryption exponent e.
+        """
+        print "Found RSA key."
+        ptr = 7
+        (n, seek) = mpi(pkt_data[ptr:]); ptr += seek
+        (e, seek) = mpi(pkt_data[ptr:]); ptr += seek
+        print "RSA n=%i, e=%i" % (n, e)
+    elif algo == 17:  # dsa
+        """
+        Algorithm-Specific Fields for DSA public keys:
+         - MPI of DSA prime p;
+         - MPI of DSA group order q (q is a prime divisor of p-1);
+         - MPI of DSA group generator g;
+         - MPI of DSA public-key value y (= g**x mod p where x
+           is secret).
+        """
+        print "Found DSA key."
+        ptr = 7
+        (p, seek) = mpi(pkt_data[ptr:]); ptr += seek
+        (q, seek) = mpi(pkt_data[ptr:]); ptr += seek
+        (g, seek) = mpi(pkt_data[ptr:]); ptr += seek
+        (y, seek) = mpi(pkt_data[ptr:]); ptr += seek
+        print "DSA params:\nP: %s,\nQ: %s,\nG: %s,\nY: %s" % (p, q, g, y)
+    elif algo == 20:
+        """
+        Algorithm-Specific Fields for Elgamal public keys:
+         - MPI of Elgamal prime p;
+         - MPI of Elgamal group generator g;
+        """
+        raise Exception("Elgamal not supported.")
 
     return (ver, created_at, algo)
 
@@ -151,8 +209,8 @@ def unarmor(pgp_msg):
     matches = pgp_header_re.finditer(pgp_msg)
 
     # Stuff between the - indicators.
-    pgp_msg_start = matches.next().end()+1
-    pgp_msg_end = matches.next().start()-1
+    pgp_msg_start = matches.next().end() + 1
+    pgp_msg_end = matches.next().start() - 1
     pgp_msg = pgp_msg[pgp_msg_start:pgp_msg_end]
 
     # Radix64 starts after two newlines.
@@ -178,5 +236,5 @@ if __name__ == "__main__":
 
     # for each packet where tag == 6
     # each packet looks like (ptr, tag, length)
-    for pub_key_pkt in [ (x, y, z) for x, y, z in pkt_index if y == 6 ]:
+    for pub_key_pkt in [(x, y, z) for x, y, z in pkt_index if y == 6]:
         print read_packet(raw_data[pub_key_pkt[0]:pub_key_pkt[2]])
