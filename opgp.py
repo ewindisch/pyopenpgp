ecbf81fada00148bd7d262aec9c746f9a008e15e
Eric Windisch <eric@cloudscaling.com>
2013-02-07 12:09:10 -0500

    SignaturePacket, subpacket in classes; Cleanup
    Also more doc changes.

--- opgp.py
+++ opgp.py
@@ -47,6 +47,14 @@ I      6        -- Public Key Packet
 """
 
 
+def mpi(buf):
+    """http://tools.ietf.org/html/rfc4880#section-3.2"""
+    length = struct.unpack(">H", buf[0:2])[0]
+    to_padded_hex = lambda n: '%0.2X' % ord(n)
+    inhexstr = ''.join(map(to_padded_hex, buf[2:2 + length]))
+    return (int('0x' + inhexstr, 0), ((length + 7) / 8) + 2)
+
+
 def get_packet_header(raw_data):
     header = ord(raw_data[0])
     fbval = header - 192
@@ -86,7 +94,6 @@ def get_packet_header(raw_data):
 def build_packet_index(raw_data):
     index = []
     ptr = 0
-    print "Length raw_data in octets: %i" % len(raw_data)
     while ptr < len(raw_data):
         (tag, length, skip) = get_packet_header(raw_data[ptr:])
 
@@ -98,135 +105,190 @@ def build_packet_index(raw_data):
     return index
 
 
-def build_subpacket_index(raw_data):
-    index = []
-    ptr = 0
-    print "Length raw_data in octets: %i" % len(raw_data)
-    while ptr < len(raw_data):
-        (tag, length, skip) = get_subpacket_header(raw_data[ptr:])
+class SignatureSubpacket(object):
+    def __init__(self, tag, data):
+        self.data = data
+        self.tag = tag
 
-        if tag == 0:
-            raise Exception("Invalid Format")
+    def tag(self):
+        return tag
 
-        index.append((tag, ptr + skip, length))
-        ptr += skip + length
-    return index
+    def value(self):
+        return self.data
 
+    @staticmethod
+    def get_header(pkt_data):
+        """Get a subpacket header from pkt_data."""
+        """http://tools.ietf.org/html/rfc4880#section-5.2.3.1"""
+        oct1 = ord(pkt_data[0])
+        ptr = 0
+        if oct1 < 192:
+            length = oct1
+            ptr += 1
+        elif oct1 >= 192 and oct1 < 255:
+            raw_length = struct.unpack('>H', pkt_data[0:2])[0]
+            length = (raw_length ^ 49152) + 192
+            ptr += 2
+        elif oct1 == 255:
+            length = struct.unpack('>L', raw_data[1:5])[0]
+            ptr += 5
 
-def mpi(buf):
-    """http://tools.ietf.org/html/rfc4880#section-3.2"""
-    length = struct.unpack(">H", buf[0:2])[0]
-    print "Length of MPI: %i" % length
-    to_padded_hex = lambda n: '%0.2X' % ord(n)
-    inhexstr = ''.join(map(to_padded_hex, buf[2:2 + length]))
-    return (int('0x' + inhexstr, 0), ((length + 7) / 8) + 2)
+        """
+        N == "need"
+        R == to support revocations
+        ? == uncertain requirement
 
-
-def get_subpacket_header(pkt_data):
-    """http://tools.ietf.org/html/rfc4880#section-5.2.3.1"""
-    oct1 = ord(pkt_data[0])
-    ptr = 0
-    if oct1 < 192:
-        length = oct1
-        ptr += 1
-    elif oct1 >= 192 and oct1 < 255:
-        raw_length = struct.unpack('>H', pkt_data[0:2])[0]
-        length = (raw_length ^ 49152) + 192
-        ptr += 2
-    elif oct1 == 255:
-        length = struct.unpack('>L', raw_data[1:5])[0]
-        ptr += 5
-
-    """
-    The value of the subpacket type octet may be:
-       2 = signature creation time
-       3 = signature expiration time
-       4 = exportable certification
-       5 = trust signature
-       6 = regular expression
-       7 = revocable
-       9 = key expiration time
-       10 = placeholder for backward compatibility
-       11 = preferred symmetric algorithms
-       12 = revocation key
-       16 = issuer key ID
-       20 = notation data
-       21 = preferred hash algorithms
-       22 = preferred compression algorithms
-       23 = key server preferences
-       24 = preferred key server
-       25 = primary user id
-       26 = policy URL
-       27 = key flags
-       28 = signer's user id
-       29 = reason for revocation
-       100 to 110 = internal or user-defined
-    """
-    pkt_type = ord(raw_data[ptr])
-    # ptr is the offset the subpacket data begins.
-    return (pkt_type, length, ptr + 1)
+        The value of the subpacket type octet may be:
+        N   2 = Signature Creation Time
+        N   3 = Signature Expiration Time
+            4 = Exportable Certification
+        N   5 = Trust Signature
+            6 = Regular Expression
+        R   7 = Revocable
+        ?   9 = Key Expiration Time
+           10 = Placeholder for backward compatibility
+           11 = Preferred Symmetric Algorithms
+        R  12 = Revocation Key
+           16 = Issuer
+           20 = Notation Data
+        ?  21 = Preferred Hash Algorithms
+           22 = Preferred Compression Algorithms
+           23 = Key Server Preferences
+           24 = Preferred Key Server
+           25 = Primary User ID
+           26 = Policy URI
+           27 = Key Flags
+           28 = Signer's User ID
+           29 = Reason for Revocation
+           30 = Features
+        R  31 = Signature Target
+        ?  32 = Embedded Signature
+           100 To 110 = Private or experimental
+        """
+        pkt_type = ord(raw_data[ptr])
+        # ptr is the offset the subpacket data begins.
+        return (pkt_type, length, ptr + 1)
 
 
-def read_signature_packet(pkt_data):
-    try:
-        version = struct.unpack('>B', pkt_data[0])[0]
-    except:
-        raise Exception("Invalid Packet")
+class SignatureSubpacketArray(object):
+    #def build_subpacket_index(raw_data):
+    def __init__(self, raw_data):
+        super(SignatureSubpacketArray, self).__init__()
 
-    if version == 3:
-        """http://tools.ietf.org/html/rfc4880#section-4.3"""
-        print "SIGPKT v3"
-        (hash_length, sig_type, created_at, key_id, algo, left16) = \
-            struct.unpack('>BBIQBBH', pkt_data[1:14])
+        index = []
+        ptr = 0
+        while ptr < len(raw_data):
+            (tag, length, skip) = SignatureSubpacket.get_header(raw_data[ptr:])
+
+            if tag == 0:
+                raise Exception("Invalid Format")
+
+            index.append((tag, ptr + skip, length))
+            ptr += skip + length
+
+        self.data = raw_data
+        self.pkt_index = index
+
+    def __iter__(self):
+        for tag, seek, length in self.pkt_index:
+            yield SignatureSubpacket(tag, self.data[seek:seek + length])
+
+    def __getitem__(self, i):
+        (tag, seek, length) = self.pkt_index[i]
+        return SignatureSubpacket(tag, self.data[seek:seek + length])
+
+
+class Packet(object):
+    pass
+
+
+class SignaturePacket(Packet):
+    def __init__(self, pkt_data):
+        try:
+            version = struct.unpack('>B', pkt_data[0])[0]
+        except:
+            raise Exception("Invalid Packet")
+
+        self.version = version
+
+        if version == 3:
+            """http://tools.ietf.org/html/rfc4880#section-4.3"""
+            (hash_length, sig_type, created_at, key_id, algo, left16) = \
+                struct.unpack('>BBIQBBH', pkt_data[1:14])
+            ptr = 15
+
+            if hash_length != 5:
+                raise Exception("Hash length must be 5 per RFC.")
+        elif version == 4:
+            """http://tools.ietf.org/html/rfc4880#section-5.2.3"""
+            (sig_type, algo, hash_algo, l_hashed_subpkts) = \
+                struct.unpack('>BBBH', pkt_data[1:6])
+            ptr = 7
+
+            # skip hashed subpkts for now.
+            #ptr += l_hashed_subpkts
+            #hashed_subpkt_index = build_subpacket_index(pkt_data[ptr:ptr +
+            #                                                     l_hashed_subpkts])
+            hashed_subpkts = SignatureSubpacketArray(pkt_data[ptr: ptr + l_hashed_subpkts])
+            ptr += l_hashed_subpkts
+
+            l_unhashed_subpkts = struct.unpack('>H', pkt_data[ptr:ptr+2])[0]
+            ptr += 2
+
+            # skip unhashed subpkts for now
+            ptr += l_unhashed_subpkts
+        else:
+            raise Exception("Unknown signature packet version.")
+
+        """Signature types:
+           - http://tools.ietf.org/html/rfc4880#section-5.2.1
+        """
+        self.signature_type = sig_type
+
+        # extract integers comprising the key material.
         ptr = 15
+        algo_fields = []
+        algoname = None
+        if algo in (1, 2, 3):  # rsa
+            """
+            Algorithm-Specific Fields for RSA signatures:
+             - multiprecision integer (MPI) of RSA signature value m**d mod n.
+            """
+            algoname = "RSA"
+            (rsa_sig, seek) = mpi(pkt_data[ptr:]); ptr += seek
+            algo_fields.append(rsa_sig)
+        elif algo == 17:  # dsa
+            """
+            Algorithm-Specific Fields for DSA signatures:
+             - MPI of DSA value r;  MPI of DSA value s.
+            """
+            algoname = "DSA"
+            (r, seek) = mpi(pkt_data[ptr:]); ptr += seek
+            (s, seek) = mpi(pkt_data[ptr:]); ptr += seek
+            algo_fields = (r, s)
 
-        if hash_length != 5:
-            raise Exception("Hash length must be 5 per RFC.")
-    elif version == 4:
-        """http://tools.ietf.org/html/rfc4880#section-5.2.3"""
-        print "SIGPKT v4"
-        (sig_type, algo, hash_algo, l_hashed_subpkts) = \
-            struct.unpack('>BBBH', pkt_data[1:6])
-        ptr = 7
+        self._algotag = algo
+        self._algoname = algoname
+        self._signature = algo_fields
+        self._hashed_subpkts = hashed_subpkts
 
-        # skip hashed subpkts for now.
-        #ptr += l_hashed_subpkts
-        hashed_subpkt_index = build_subpacket_index(pkt_data[ptr:ptr +
-                                                             l_hashed_subpkts])
+    def algorithm(self):
+        """RSA or DSA"""
+        return self._algoname
 
-        l_unhashed_subpkts = struct.unpack('>H', pkt_data[ptr:ptr+2])[0]
-        ptr += 2
+    def tag(self):
+        return self._aglotag
 
-        # skip unhashed subpkts for now
-        ptr += l_unhashed_subpkts
-    else:
-        raise Exception("Unknown signature packet version.")
-
-    print "Signature type: %0.2X" % sig_type
-
-    # extract integers comprising the key material.
-    ptr = 15
-    algo_fields = []
-    if algo in (1, 2, 3):  # rsa
+    def signature(self):
         """
-        Algorithm-Specific Fields for RSA signatures:
-         - multiprecision integer (MPI) of RSA signature value m**d mod n.
+           Return signature.
+            if rsa: value m**d mod n
+            if dsa: (r, s)
         """
-        print "Found RSA signature."
-        (rsa_sig, seek) = mpi(pkt_data[ptr:]); ptr += seek
-        algo_fields.append(rsa_sig)
-        print "RSA sig=%i" % (rsa_sig, )
-    elif algo == 17:  # dsa
-        """
-        Algorithm-Specific Fields for DSA signatures:
-         - MPI of DSA value r;  MPI of DSA value s.
-        """
-        print "Found DSA signature."
-        (r, seek) = mpi(pkt_data[ptr:]); ptr += seek
-        (s, seek) = mpi(pkt_data[ptr:]); ptr += seek
-        print "DSA params:\nR: %s,\nS: %s" % (r, s)
-        algo_fields = (r, s)
-    return (algo_fields, hashed_subpkt_index)
+        return self._signature
+
+    def hashed_subpackets(self):
+        return self._hashed_subpkts
 
 
 def read_public_key_packet(pkt_data):
@@ -256,11 +318,10 @@ def read_public_key_packet(pkt_data):
         - multiprecision integer (MPI) of RSA public modulus n;
         - MPI of RSA public encryption exponent e.
         """
-        print "Found RSA key."
         ptr = 7
         (n, seek) = mpi(pkt_data[ptr:]); ptr += seek
         (e, seek) = mpi(pkt_data[ptr:]); ptr += seek
-        print "RSA n=%i, e=%i" % (n, e)
+        #print "RSA n=%i, e=%i" % (n, e)
     elif algo == 17:  # dsa
         """
         Algorithm-Specific Fields for DSA public keys:
@@ -270,13 +331,12 @@ def read_public_key_packet(pkt_data):
          - MPI of DSA public-key value y (= g**x mod p where x
            is secret).
         """
-        print "Found DSA key."
         ptr = 7
         (p, seek) = mpi(pkt_data[ptr:]); ptr += seek
         (q, seek) = mpi(pkt_data[ptr:]); ptr += seek
         (g, seek) = mpi(pkt_data[ptr:]); ptr += seek
         (y, seek) = mpi(pkt_data[ptr:]); ptr += seek
-        print "DSA params:\nP: %s,\nQ: %s,\nG: %s,\nY: %s" % (p, q, g, y)
+        #print "DSA params:\nP: %s,\nQ: %s,\nG: %s,\nY: %s" % (p, q, g, y)
     elif algo == 20:
         """
         Algorithm-Specific Fields for Elgamal public keys:
@@ -310,7 +370,6 @@ if __name__ == "__main__":
     key_lines = sys.stdin.readlines()
     pgp_msg = ''.join(key_lines)
     raw_data = unarmor(pgp_msg)
-    #raw_data = pgp_msg
 
     pkt_index = build_packet_index(raw_data)
 
@@ -323,4 +382,19 @@ if __name__ == "__main__":
     # each packet looks like (tag, ptr, length)
     for pub_key_pkt in [(x, y, z) for x, y, z in pkt_index if x == 2]:
         print pub_key_pkt
-        print read_signature_packet(raw_data[pub_key_pkt[1]:pub_key_pkt[1]+pub_key_pkt[2]])
+        print "Signature packet:"
+        sp = SignaturePacket(raw_data[pub_key_pkt[1]:pub_key_pkt[1]+pub_key_pkt[2]])
+
+        print "Signature version: %i" % sp.version
+        print "Signature type: %i" % sp.signature_type
+        print "Signature uses %s" % sp.algorithm()
+
+        print "-----START SIGNATURE-----"
+        print map(base64.b64encode, map(str, sp.signature()))
+        print "----- END  SIGNATURE-----"
+
+        print "Subpackets:"
+        for pkt in sp.hashed_subpackets():
+            print "-----SUBPACKET START-----"
+            print base64.b64encode(pkt.value())
+            print "-----SUBPACKET   END-----"
